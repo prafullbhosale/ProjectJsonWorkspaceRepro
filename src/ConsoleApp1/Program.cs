e4e7078feec2256f6aaf0bb3d75cb4fff9d91b9b
Prafull Bhosale <prbhosal@microsoft.com>
2016-06-17 10:54:54 -0700

    Add repro projects

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.ProjectModel;
+using Microsoft.DotNet.ProjectModel.Workspaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var projectFile = ProjectReader.GetProject(@"..\ConsoleApp2\project.json");
+            ProjectContext context = new ProjectContextBuilder()
+                .WithProject(projectFile)
+                .WithTargetFramework(NuGet.Frameworks.FrameworkConstants.CommonFrameworks.NetCoreApp10)
+                .Build();
+
+            var workspace = context.CreateRoslynWorkspace();
+            var compilation = workspace.CurrentSolution.Projects.First().GetCompilationAsync().Result;
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms, pdbStream: null);
+                if (!result.Success)
+                {
+                    var formatter = new DiagnosticFormatter();
+                    var errorMessages = result.Diagnostics
+                                            .Where(IsError)
+                                            .Select(d => formatter.Format(d));
+                    Console.WriteLine("ErrorMessage:: ");
+                    foreach(var e in errorMessages)
+                    {
+                        Console.WriteLine($"    {e}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Compilation succeeded");
+                }
+            }
+        }
+
+        private static bool IsError(Diagnostic diagnostic)
+        {
+            return diagnostic.Severity == DiagnosticSeverity.Error;
+        }
+    }
+}
---
+++ src/ConsoleApp2/Class.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Class
+    {
+        public class Cloud
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+
+            private CloudBlobContainer _container;
+
+            public Task<CloudBlobContainer> GetCloudBlobContainer()
+            {
+                return Task.FromResult(_container);
+            }
+
+            public Cloud()
+            {
+                _container = new CloudBlobContainer(new Uri(""));
+            }
+
+            public async Task ThisMakesItBreak()
+            {
+                var container = await GetCloudBlobContainer();
+                await container.SetPermissionsAsync(new BlobContainerPermissions
+                {
+                    //PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+            }
+        }
+    }
+}
